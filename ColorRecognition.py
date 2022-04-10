7b73267aefc75acf70a6ac3822755f2790218c6d
Max Base <MaxBaseCode@gmail.com>
2022-04-10 17:52:24 +0430

    clean code

--- ColorRecognition.py
+++ ColorRecognition.py
@@ -12,19 +12,22 @@ r = g = b = xpos = ypos = 0
 def recognize_color(R, G, B):
     minimum = 10000
     for i in range(len(csv)):
-        d = abs(R- int(csv.loc[i,'R'])) + abs(G- int(csv.loc[i,'G']))+ abs(B- int(csv.loc[i,'B']))
-        if(d<=minimum):
+        d = abs(R - int(csv.loc[i,'R'])) + abs(G - int(csv.loc[i,'G'])) + abs(B - int(csv.loc[i,'B']))
+        if d <= minimum:
             minimum = d
             cname = csv.loc[i,'color_name']
     return cname
 
 def mouse_click(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b,g,r,xpos,ypos, clicked
+        global b, g, r, xpos, ypos, clicked
+
         clicked = True
         xpos = x
         ypos = y
-        b,g,r = img[y,x]
+
+        b, g, r = img[y,x]
+
         b = int(b)
         g = int(g)
         r = int(r)
