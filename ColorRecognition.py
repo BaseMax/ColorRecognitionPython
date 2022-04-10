a56c514d6cfeff590d2a959cdc71d7101b89c344
Max Base <MaxBaseCode@gmail.com>
2022-04-10 17:41:48 +0430

    create py

---
+++ ColorRecognition.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import pandas as pd
+
+# Global variables
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+
+r = g = b = xpos = ypos = 0
+
+def recognize_color(R,G,B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
+
+def mouse_click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+# Startup
+cv2.namedWindow('Color Recognition App')
+cv2.setMouseCallback('Color Recognition App', mouse_click)
+
+img = cv2.imread("color_image.jpg")
+
+while(1):
+    cv2.imshow("Color Recognition App",img)
+    if (clicked):
+        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+        text = recognize_color(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+        if(r + g + b >= 600):
+            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+            
+        clicked=False
+        #Break the loop when user hits 'esc' key    
+        if cv2.waitKey(20) & 0xFF ==27:
+            break
+
+cv2.destroyAllWindows()
