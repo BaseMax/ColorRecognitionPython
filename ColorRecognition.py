3e0d06bc17da124c83f130ce76b3e3101a69de65
Max Base <MaxBaseCode@gmail.com>
2022-04-10 17:50:18 +0430

    clean code

--- ColorRecognition.py
+++ ColorRecognition.py
@@ -9,7 +9,7 @@ csv = pd.read_csv('colors.csv', names=index, header=None)
 clicked = False
 r = g = b = xpos = ypos = 0
 
-def recognize_color(R,G,B):
+def recognize_color(R, G, B):
     minimum = 10000
     for i in range(len(csv)):
         d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
@@ -41,14 +41,14 @@ while 1:
         cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
 
         text = recognize_color(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(img, text, (50,50), 2, 0.8, (255,255,255), 2, cv2.LINE_AA)
 
         if r + g + b >= 600:
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+            cv2.putText(img, text, (50,50), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
             
         clicked = False
 
-    if cv2.waitKey(20) & 0xFF ==27:
+    if cv2.waitKey(20) & 0xFF == 27:
         break
 
 cv2.destroyAllWindows()
